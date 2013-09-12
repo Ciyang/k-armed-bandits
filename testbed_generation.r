1799fded22c7f9b48591e3528a2c732f9794aaa0
Ciyang <Ciyang@users.noreply.github.com>
2013-09-12 18:52:48 +0200

    Update testbed_generation.r

--- testbed_generation.r
+++ testbed_generation.r
@@ -1,6 +1,7 @@
-# generate a testbed of n k-armed bandits
-# A bandit is a vector of length k, whose entry is the mean reward a
-# a ~ N(0,1)
+# Generate a testbed of n k-armed bandits
+# A bandit is a vector of length k, each of whose entries 
+# is the mean reward r for the corresponding arm
+# r ~ N(0,1)
 
 # A testbed is a matrix, each row of which is an k-armed bandit
 
@@ -10,7 +11,7 @@ testbed.generate <- function(n, k){
                   )
 }
 
-
+# Create a testbed of 2000 10-armed bandits, as in S&B's book
 tbed <- testbed.generate(2000, 10)
 
 write.csv(tbed,"testbed.csv",row.names=FALSE)
