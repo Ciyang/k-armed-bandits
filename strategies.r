3405c08a99877cc4dafe8243d54bac2d6412e68e
Ciyang <ciyang@server.fake>
2013-09-12 18:19:37 +0200

    Copy what I have so far.

---
+++ strategies.r
@@ -0,0 +1,16 @@
+# We maintain the belief-goal-action view.
+
+# The goal can be either directly from the environment,
+# or implicitly assumed by the decision rule.
+
+# A strategy actually consists of (at least) two parts:
+# the decision rule, which takes in a belief and outputs an action,
+# and a belief revision rule, which takes in the current belief
+# and the new information, and outputs a new belief
+
+# In this sense, the action-value methods all share 
+# the same belief revision rule.
+
+
+
+
